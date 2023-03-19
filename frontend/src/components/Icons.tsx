ce06b074cf5a1ada029ad5612870a22206b2c80c
Podskio <podskio1@gmail.com>
2023-03-18 22:12:54 -0700

    Add chevrons and spinner

---
+++ frontend/src/components/Icons.tsx
@@ -0,0 +1,50 @@
+export const ChevronUp = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    width={20}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4.5 15.75l7.5-7.5 7.5 7.5"
+    />
+  </svg>
+)
+
+export const ChevronDown = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    width={20}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+    />
+  </svg>
+)
+
+export const Spinner = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 17 17"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    width={20}
+  >
+    <path
+      d="M16 8.5C16 6.96355 15.5281 5.46417 14.6482 4.20465C13.7682 2.94514 12.5227 1.98627 11.08 1.45773C9.63732 0.929189 8.06712 0.856487 6.58177 1.24946C5.09642 1.64242 3.7676 2.4821 2.77504 3.65492C1.78248 4.82773 1.17406 6.27709 1.03209 7.80697C0.890114 9.33685 1.22143 10.8734 1.98123 12.2089C2.74102 13.5443 3.89264 14.6142 5.28031 15.2737C6.66797 15.9333 8.22474 16.1508 9.74004 15.8968"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
